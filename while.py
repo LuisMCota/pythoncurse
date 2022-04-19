df79bf33fcb7027af2ddf60bc9aaa4e8ce5b1962
LuisMCota <luis.f.m.c@outlook.com>
2022-04-19 09:39:07 -0500

    commit

---
+++ while.py
@@ -0,0 +1,9 @@
+#while loops = A statement that will execute it's block of code,
+#              as long as it's condition remains true
+
+name = None
+#entra en  un loop hasta que pones un nombre
+while not name:
+    name  = input("Enter your name: ") 
+
+print("Hello "+ name)
