e7f3011a6f6644c8ad62e4f2d813834e710af33c
LuisMCota <luis.f.m.c@outlook.com>
2022-04-19 16:02:02 -0500

    commit

--- nestedloop.py
+++ nestedloop.py
@@ -1,2 +1,11 @@
 #nested loop = The "Ineer loop" will finish all of it's iterations before 
 #              finishing one iteration of the "outer loop"
+
+rows = int(input("How many rows? "))
+columns = int(input("How many columns? "))
+symbol = input("Enter a Symmbolto use: ")
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end = "")#se introduce una linea nueva de caracteres debido a que es un print
+    print()
