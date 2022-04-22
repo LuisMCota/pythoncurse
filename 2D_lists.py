3570a4c4d464575a4767b48e49a3b597ea1cbef8
LuisMCota <luis.f.m.c@outlook.com>
2022-04-22 11:27:16 -0500

    commit

---
+++ 2D_lists.py
@@ -0,0 +1,10 @@
+# 2D lists = a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food  = [drinks, dinner, dessert] # junta las listas
+print(food) 
+print(food[1]) # se pone un index, solo imprime la lista seleccionada
+print(food[1][1]) # imprime el elemnto 1 de la lista 1
--- list.py
+++ list.py
@@ -1,4 +1,18 @@
 # list = used to store mulltiple items in a single variable
 
 food = ["pizza", "hamburger", "hot dog", "spaghetti"]
+food[0] = "sushi" # cambia el elemnto 0 por sushi
 
+food.append("ice cream") # para agregar un valor a la lista
+food.remove("hog dog") # elimina el valor 
+food. pop() # elimina el ultimo elemento
+food.insert(0, "cake") # cambia el elemnto selecionado por el elemnto escrito
+food.sort() # sortea elementos dentro de la lista
+food.clear()# limpia la la lista
+
+
+
+print(food[3]) # inprime el numero de la lista que escribes
+
+for x in food:
+    print(x) # sirve para imprimir toda la lista
