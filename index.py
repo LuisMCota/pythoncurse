2d27b9963a2361d4d59309966954273ca79f033a
LuisMCota <luis.f.m.c@outlook.com>
2022-04-25 23:38:08 -0500

    git commit

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
 # index operator [] = gives access to a sequence element (str, list, tuples)
+
+name = "luis Cota !"
+
+if(name[0].islower()):
+    name = name.capitalize()
+print(name)
+
+print(name[:4].upper())
+print(name[4:].lower())
+print(name[-1])
