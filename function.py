81813c449c02f031d2d8278b780c10602b969bd3
Luis Cota <luis.f.m.c@outlook.com>
2022-05-10 13:20:00 -0500

    commit

---
+++ function.py
@@ -0,0 +1,8 @@
+# function = a block of code which is executed only when it is called.
+
+def hello(first_name, last_name, age): # son los parametros de la funcion
+    print("Hello " + first_name + " " + last_name)
+    print("Have a nice day! ")
+    print("You are " + str(age)) #hasta aqui acaba la funcion
+    
+hello("Luis", "Cota", 21)# aqui probamos la funcion con los argumentos
---
+++ key_arguments.py
@@ -0,0 +1,3 @@
+# keywords arguments = arguments preceded by an identifier when we pass them to a function
+#                      The order of the arguments doesnt matter, unlike positional arguments
+#                      Python knows the names of the arguments that our function
